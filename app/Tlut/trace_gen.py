57063304ea9cd8a083e739abc57d059f84bcf733
Zhewen <zhewen.pan@wisc.edu>
2021-11-27 08:02:38 -0600

    Adding inital trace gen and more configs

--- app/Tlut/trace_gen.py
+++ app/Tlut/trace_gen.py
@@ -14,6 +14,7 @@ import pathlib
 from tracegen_parse import Prob, Arch
 import math
 import utils
+import numpy as np
 _TRANCEGEN_DIR = os.environ['TRANCEGEN_DIR']
 
 def construct_argparser():
@@ -35,7 +36,7 @@ def construct_argparser():
                         '--prob_path',
                         type=str,
                         help='Problem Dimension Path',
-                        default=f'{_TRANCEGEN_DIR}/configs/workloads/alexnet_graph/_outputs_input.2.yaml',
+                        default=f'{_TRANCEGEN_DIR}/configs/workloads/convnet_graph/_conv1.yaml',
                         )
 
     parser.add_argument('--dataflow',
@@ -61,8 +62,8 @@ def cg_profile(prob, arch, output_dir, dataflow):
 
     # tiling_factors: (input, weight) dimension tiling consts
     pqn = prob.prob_bound[prob.prob_name_idx_dict['P']]*prob.prob_bound[prob.prob_name_idx_dict['Q']]*prob.prob_bound[prob.prob_name_idx_dict['N']]
-    k = prob.prob_bound[prob.prob_name_idx_dict['K']]
-    tiling_factors = (math.ceil(k/arch.arithmetic['dimC']), 
+    K = prob.prob_bound[prob.prob_name_idx_dict['K']]
+    tiling_factors = (math.ceil(K/arch.arithmetic['dimC']), 
         math.ceil(pqn/arch.arithmetic['dimA']))
     spatially_remapping = tiling_factors[0] * tiling_factors[1]
     temporal_mapping_times = spatially_remapping * prob.prob_bound[prob.prob_name_idx_dict['R']]*prob.prob_bound[prob.prob_name_idx_dict['S']]*prob.prob_bound[prob.prob_name_idx_dict['C']]
@@ -74,7 +75,7 @@ def cg_profile(prob, arch, output_dir, dataflow):
     latency = temporal_mapping_times * single_pass_latency
     print("latency = ", latency)
     total_compute = spatially_remapping * arch.arithmetic['dimA'] * arch.arithmetic['dimC']
-    utilized_compute = pqn * k
+    utilized_compute = pqn * K
     utilization = utilized_compute / total_compute * 100
 
     # parse coarse grained stats to json file
@@ -90,11 +91,84 @@ def cg_profile(prob, arch, output_dir, dataflow):
     utils.store_json(json_file, status_dict, indent=4)
 
     #****************gen trace*************
-    hw_w = arch.arithmetic.dimC
-    hw_i = arch.arithmetic.dimA
+    # output file path
+    sram_read_trace_file = output_dir / 'sram_read.csv'
+    rd_outfile = open(sram_read_trace_file, 'w')
+    sram_write_trace_file = output_dir / 'sram_write.csv'
+    wr_outfile = open(sram_write_trace_file, 'w')
 
-    # for n in range(prob.prob_bound[prob.prob_name_idx_dict['N']]):
-    
+    hw_w = arch.arithmetic['dimC']
+    hw_i = arch.arithmetic['dimA']
+    R = prob.prob_bound[prob.prob_name_idx_dict['R']]
+    S = prob.prob_bound[prob.prob_name_idx_dict['S']]
+    C = prob.prob_bound[prob.prob_name_idx_dict['C']]
+    N = prob.prob_bound[prob.prob_name_idx_dict['N']]
+    P = prob.prob_bound[prob.prob_name_idx_dict['P']]
+    Q = prob.prob_bound[prob.prob_name_idx_dict['Q']]
+    Wstride = prob.prob['Wstride']
+    Hstride = prob.prob['Hstride']
+    Wdilation = prob.prob['Wdilation']
+    Hdilation = prob.prob['Hdilation']
+    PAD = 0
+    WGHT = K * R * S * C
+    W = (P-1)*Wstride + R + 2 * PAD 
+    H = (Q-1)*Hstride + S + 2 * PAD 
+    INP = N * P * Q
+
+    # address base (byte addressable) Note: different from uSystolic
+    output_base=2000000 # output feature map base addr, in byte
+    wght_base=1000000 # weight base addr, in byte
+    input_base=0 # input feature map base addr, in byte
+
+    cycle = 0
+    i_pass = 0 # index of passes
+
+    # load wght
+    wght_addr = []
+    if hw_w < WGHT: w_bound = hw_w
+    else: w_bound = WGHT
+    for i in range(w_bound):
+        wght_addr.append(wght_base + i*R*S*C) # TODO: fix flex weight memory layout
+    wght_rd = f'{cycle},' + utils.list_to_comma_separated_str_with_padding(wght_addr, hw_w)
+    rd_outfile.write(wght_rd)
+
+    # load input
+    input_addr = []
+    if hw_i < INP: 
+        i_bound = hw_i
+        if hw_i < P:
+            p_bound = hw_i
+            q_bound = 1
+            n_bound = 1
+        else: 
+            p_bound = P
+            if hw_i < P*Q:
+                q_bound = int(hw_i/P)
+                n_bound = 1
+            else: 
+                q_bound = Q
+                n_bound = int(hw_i/(P*Q))
+    else: 
+        i_bound = INP
+        p_bound = P # p subbound
+        q_bound = Q # q subbound
+        n_bound = N # n subbound
+    print(f'Debugging input bounds\np:{p_bound}, q:{q_bound}, n:{n_bound}')
+    # image to column address indexing
+    input_layout = arch.storage[arch.mem_idx['InputBuffer']]['layout']
+    print(f'Debugging input layout: {input_layout}')
+    input_addr = []
+    for p in range(p_bound):
+        for q in range(q_bound):
+            for n in range(n_bound):
+                new_addr = input_base + utils.im2col_addr( #TODO: fix flex input memory layout
+                    input_layout=input_layout,
+                    patch_P=p, patch_Q=q, patch_N=n, pixel=i_pass, pad=PAD, R=R, S=S, C=C, N=N,
+                    Wdilation=Wdilation, Hdilation=Hdilation, Wstride=Wstride, Hstride=Hstride)
+                input_addr.append(new_addr)
+    input_rd = f'{cycle},' + utils.list_to_comma_separated_str_with_padding(input_addr, hw_i)
+    rd_outfile.write(input_rd)
+
 
 def run_trace_gen(prob_path, arch_path, output_path, dataflow):
     prob = Prob(prob_path)
@@ -105,7 +179,7 @@ def run_trace_gen(prob_path, arch_path, output_path, dataflow):
     arch.print()
 
     cg_profile(prob, arch, output_path, dataflow)
-    gen(prob, arch, output_path, dataflow)
+    # gen(prob, arch, output_path, dataflow)
 
 
 if __name__ == "__main__":
--- app/Tlut/utils.py
+++ app/Tlut/utils.py
@@ -1,3 +1,4 @@
+from math import pi
 import yaml
 import json
 
@@ -9,3 +10,39 @@ def parse_yaml(yaml_path):
 def store_json(json_path, data, indent=None):
     with open(json_path, "w") as f:
         json.dump(data, f, indent=indent)
+
+def list_to_comma_separated_str_with_padding(_list, dim_hw):
+    assert(len(_list) <= dim_hw)
+    _str = ''
+    # for item in _list: _str += f'{item},'
+    for i in range(dim_hw): 
+        if i < len(_list): _str += f'{_list[i]},'
+        else: 
+            _str += 'nan,'
+    _str += '\n'
+    return _str
+
+def im2col_addr(input_layout, patch_P, patch_Q, patch_N, pixel, pad, 
+    R, S, C, N, Wdilation, Hdilation, Wstride, Hstride):
+    # RSC along a col, pixel is the col index
+    assert input_layout == 'NWHC' #TODO: implemet flex input layout
+    # pixel = 9 # set override val for debugging
+    c_index = int(pixel/(R*S))
+    s_index = int(pixel/R % R)
+    r_index = pixel % R
+    # print(f'({r_index}, {s_index}, {c_index})')
+
+    w_from_P = (patch_P - 1 -1) * Wstride + R - 2 * pad
+    h_from_Q = (patch_Q - 1-1) * Hstride + S - 2 * pad
+
+    w_from_P_index = 1
+    h_from_Q_index = 1
+
+    w_index = w_from_P + r_index - w_from_P_index
+    h_index = h_from_Q + s_index - h_from_Q_index
+
+    # print(f'{w_index}, {h_index}, {c_index}')
+    addr = (R*S)*c_index + (R)*h_index + (1)*w_index
+    # print(addr)
+    
+    return addr
